b834357bbb42a82b40669e238088d76778c114fb
sddrozd <sddrozd@A24COMP7>
2018-10-08 20:12:16 +0300

    0.0.1

---
+++ Black Jack/Cards.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Black_Jack
+{
+    class Cards
+    {
+        public string Name { get; set; }
+        public int Value { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"Name = {Name}, Value = {Value}";
+        }
+    }
+}
